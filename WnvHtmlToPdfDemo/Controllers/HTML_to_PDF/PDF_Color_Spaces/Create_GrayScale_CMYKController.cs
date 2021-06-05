e8e7d86ef8ad9323ae9a9d1d6d6ca9632d3ba5a1
Sharp Ninja <ninja@thesharp.ninja>
2021-06-05 12:09:53 -0500

    Initial Checkin

---
+++ WnvHtmlToPdfDemo/Controllers/HTML_to_PDF/PDF_Color_Spaces/Create_GrayScale_CMYKController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+// Use Winnovative Namespace
+using Winnovative;
+
+namespace WnvHtmlToPdfDemo.Controllers.HTML_to_PDF.PDF_Color_Spaces
+{
+    public class Create_GrayScale_CMYKController : Controller
+    {
+        // GET: Create_GrayScale_CMYK
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ConvertHtmlToPdf(IFormCollection collection)
+        {
+            // Create a HTML to PDF converter object with default settings
+            HtmlToPdfConverter htmlToPdfConverter = new HtmlToPdfConverter();
+
+            // Set license key received after purchase to use the converter in licensed mode
+            // Leave it not set to use the converter in demo mode
+            htmlToPdfConverter.LicenseKey = "fvDh8eDx4fHg4P/h8eLg/+Dj/+jo6Og=";
+
+            // Set an adddional delay in seconds to wait for JavaScript or AJAX calls after page load completed
+            // Set this property to 0 if you don't need to wait for such asynchcronous operations to finish
+            htmlToPdfConverter.ConversionDelay = 2;
+
+            // Set the PDF color space
+            // By default the RGB color space is used
+            if (collection["PdfColorSpace"] == "grayScaleRadioButton")
+                htmlToPdfConverter.PdfDocumentOptions.ColorSpace = ColorSpace.Gray;
+            else if (collection["PdfColorSpace"] == "cmykRadioButton")
+                htmlToPdfConverter.PdfDocumentOptions.ColorSpace = ColorSpace.CMYK;
+
+            // Convert the HTML page to a PDF document in a memory buffer
+            byte[] outPdfBuffer = htmlToPdfConverter.ConvertUrl(collection["urlTextBox"]);
+            
+            // Send the PDF file to browser
+            FileResult fileResult = new FileContentResult(outPdfBuffer, "application/pdf");
+            fileResult.FileDownloadName = "PDF_Color_Space.pdf";
+
+            return fileResult;
+        }
+    }
+}
